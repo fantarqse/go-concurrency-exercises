3f9d5b3ddf58f751d7be33aa96f8e45cf5c5cbc8
fantarqse <fantarose51@gmail.com>
2024-06-16 14:11:09 +0300

    feat(3-limit-service-time): solved

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,19 +10,54 @@
 
 package main
 
+import (
+	"sync"
+	"time"
+)
+
+const maxTime time.Duration = 10
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
+	mu        sync.Mutex
 }
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	if u.IsPremium { // checks premium
+		process()
+		return true
+	}
+	u.mu.Lock()
+	defer u.mu.Unlock()
+
+	if u.TimeUsed >= int64(maxTime) {
+		return false
+	}
+
+	done := make(chan time.Time, 1)
+	start := time.Now()
+
+	go func() {
+		process()
+		done <- time.Now()
+	}()
+
+	select {
+	case end := <-done: // a process completed
+		duration := end.Sub(start)
+		u.TimeUsed += int64(duration.Seconds())
+
+		return true
+	case <-time.After(time.Duration(10-u.TimeUsed) * time.Second): // a process aborted
+		u.TimeUsed = 10
+		return false
+	}
 }
 
 func main() {
